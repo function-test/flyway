20c24b55faa55b9efd4414d7c255726a1396b075
MJ Youn <yun0244@naver.com>
2019-05-27 13:32:47 +0900

    init

---
+++ src/main/resources/db/migration/V1.1__Insert_Person_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO
+	person(ID,NAME,EMAIL,PHONE)
+VALUES
+	(1, 'kim', 'aaa@xxx.kr', '010-1234-5678');
---
+++ src/main/resources/db/migration/V1.2__Create_Address_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE address (
+	id int not null,
+	mp varchar(10) not null,
+	ccd varchar(10) not null,
+	emd varchar(10) not null,
+	iag int not null,
+	lat int not null
+);
